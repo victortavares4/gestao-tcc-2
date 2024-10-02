7286009d6ec764dbdfd8776d9098ea91abc544ec
nicolaslopess <nicolaslopes3@mx2.unisc.br>
2024-10-02 00:58:17 -0300

    feat(Reuniao): endpont

--- gestao-tcc/src/main/java/br/com/gestaotcc/gestaotcc/resources/service/api/reiniao/ReuniaoController.java
+++ gestao-tcc/src/main/java/br/com/gestaotcc/gestaotcc/resources/service/api/reiniao/ReuniaoController.java
@@ -4,7 +4,20 @@
  */
 package br.com.gestaotcc.gestaotcc.resources.service.api.reiniao;
 
+import br.com.gestaotcc.gestaotcc.utils.StandardResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -13,4 +26,17 @@ import javax.ws.rs.Path;
 @Path("/reuniao")
 public class ReuniaoController {
     
+    @GET
+    @Path("/findall/{tipo}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllTipo(@PathParam("tipo") String tipo) {
+
+        try {
+           
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(new StandardResponse(e.getMessage())).build();
+        }
+    }
 }
